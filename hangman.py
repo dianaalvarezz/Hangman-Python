d186d0b3a15b7bfea4f7b94bda47bccc645f7c37
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-02-21 09:48:24 -0600

    Update hangman.py
    started to add some documentation

--- hangman.py
+++ hangman.py
@@ -7,10 +7,13 @@ Date: 2024-02-201
 inport random
 from collections import Counter
 
+# String that will serve as word bank for game
 wordBank = '''monkey apple banana chicken bed table kevin diana danisha computer coding engineering SHPE SWE robotics juice '''
 
+# Splits the string into words at the spaces
 wordBank = wordBack.split(' ')
 
+# Selects a word randomly from the word bank
 word = random.choice(wordBank)
 
 if __name__ == '__main__':
@@ -27,4 +30,10 @@ if __name__ == '__main__':
     correct = 0
     wrong = 0
 
-    
+    try:
+        while (chances != 0) and wrong == 0: 
+            print()
+            chances -=1
+
+            try: guess  =
+
