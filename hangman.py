495d067caeade0043188583775ccd500173700e3
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-06-15 22:16:29 -0500

    Update hangman.py
    Made a note to remind me to come back and what to work on

--- hangman.py
+++ hangman.py
@@ -3,6 +3,8 @@ Hangman Game
 This script allows the user to play a simple version of Hangman
 Author: Diana Alvarez
 Date: 2024-02-201
+
+FIX THIS
 """
 import random
 from collections import Counter
